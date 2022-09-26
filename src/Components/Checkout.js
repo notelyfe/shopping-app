f28960f4bfd45489270a791ec61a03d6f5857d15
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-09-26 19:06:12 +0530

    css updated

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -4,7 +4,7 @@ import './Checkout.css'
 
 const Checkout = () => {
     return (
-        <div className="container">
+        <div className="container d-flex mt-2">
             <table className="table my-3 checkout-data">
                 <thead className='bg-light text-center'>
                     <tr>
@@ -43,6 +43,21 @@ const Checkout = () => {
                     </tr>
                 </tbody>
             </table>
+
+            <div className="container rounded cart-amount-total ">
+                <h2 className='mb-3 mt-1'>Cart Total</h2>
+                <div className=' d-flex justify-content-between align-items-center' style={{ marginBottom: '-15px'}}>
+                    <h6 >Subtotal</h6>
+                    <h6>$101.00</h6>
+                </div>
+                <hr />
+                <div className="d-flex justify-content-between align-items-center">
+                    <h3>Total</h3>
+                    <h3 className='text-primary'>$101.00</h3>
+                </div>
+                <button className="btn my-2 btn-primary btn-add">PROCEED TO CHECKOUT</button>
+            </div>
+
         </div>
     )
 }
